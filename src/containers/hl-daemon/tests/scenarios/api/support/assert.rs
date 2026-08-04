b9069e3cc8b722b0995899a3a68a17401202ad78
Richard Huttar <huttarichard@icloud.com>
2026-08-03 20:26:10 -0700

    refactor: finish scenario test relocation

--- src/containers/hl-daemon/tests/scenarios/api/support/assert.rs
+++
@@ -1,9 +0,0 @@
-//! Assertion helper shared by daemon integration tests.
-
-pub(crate) fn require(value: bool, message: &str) -> Result<(), Box<dyn std::error::Error>> {
-    if value {
-        Ok(())
-    } else {
-        Err(message.into())
-    }
-}
