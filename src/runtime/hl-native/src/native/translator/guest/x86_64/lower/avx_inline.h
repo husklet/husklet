000f0adbd75064891ec92d8662deb299dff4acc0
Richard Huttar <huttarichard@icloud.com>
2026-08-12 19:45:03 -0700

    refactor(native): extract inline AVX lowering

---
+++ src/runtime/hl-native/src/native/translator/guest/x86_64/lower/avx_inline.h
@@ -0,0 +1,10 @@
+#ifndef HL_TRANSLATOR_GUEST_X86_64_LOWER_AVX_INLINE_H
+#define HL_TRANSLATOR_GUEST_X86_64_LOWER_AVX_INLINE_H
+
+#include <stdint.h>
+
+#include "../decoder.h"
+
+int hl_x86_lower_avx_inline(struct insn *, uint64_t);
+
+#endif
