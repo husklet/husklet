0a98f44102aa1addb596e60dd296cdd2dc691090
Richard Huttar <huttarichard@icloud.com>
2026-08-18 10:58:15 -0700

    fix(native): include namespace transaction once

--- src/runtime/hl-native/src/native/linux_abi/container/vfs.c
+++ src/runtime/hl-native/src/native/linux_abi/container/vfs.c
@@ -2,7 +2,6 @@
 #include "vfs/path_compose.h"
 #include "vfs/namespace_transaction.c"
 #include "vfs/path_runtime.c"
-#include "vfs/namespace_transaction.c"
 #include "vfs/mounts.c"
 #include "vfs/process_maps.c"
 #include "vfs/process_registry.c"
