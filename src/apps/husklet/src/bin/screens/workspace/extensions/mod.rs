e291a1429d4457446a3a12a9a185a691eeb8c0e2
Richard Huttar <huttarichard@icloud.com>
2026-09-08 07:38:06 +0000

    Update GUI extension manifest fixture

--- src/apps/husklet/src/bin/screens/workspace/extensions/mod.rs
+++ src/apps/husklet/src/bin/screens/workspace/extensions/mod.rs
@@ -223,6 +223,7 @@ mod tests {
             filesystem: hl_extension::FilesystemGrant::default(),
             workspace_environment: hl_extension::WorkspaceEnvironmentGrant::default(),
             containers: hl_extension::ContainerGrant::default(),
+            networks: hl_extension::NetworkGrant::default(),
         }
     }
 
